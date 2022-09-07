bd46d10499cea7f4608029d39b5687f04463be54
Ken Nguyen <nln.vn.it@gmail.com>
2022-09-07 17:06:46 +0700

    add edit page

---
+++ apps/employee-management/pages/employee/edit/[id].tsx
@@ -0,0 +1,7 @@
+import { EditEmployee } from "../../src/components/edit-employee";
+
+const EditEmployeePage = () => {
+  return <EditEmployee></EditEmployee>;
+};
+
+export default EditEmployeePage;
